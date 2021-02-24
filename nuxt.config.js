0d19648b7a77aaa9193c74c143c8687d0f39c868
Errol <errolmasuncion@gmail.com>
2021-02-24 15:28:39 +0800

    add header and initial index page
    Signed-off-by: Errol <errolmasuncion@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,15 +8,23 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Rewards & Recognition',
-    htmlAttrs: {
-      lang: 'en',
-    },
+    htmlAttrs: {lang: 'en'},
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Global Education Rewards & Recognition v2 web application'
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
+    ],
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
@@ -27,9 +35,9 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    // '@nuxtjs/eslint-module',
+    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
-    // '@nuxtjs/stylelint-module',
+    '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
   ],
