5703ec6b453b2fad233994f3213a4a54f5f4b933
Errol <errolmasuncion@gmail.com>
2021-02-24 15:28:39 +0800

    add header and initial index page
    Signed-off-by: Errol <errolmasuncion@gmail.com>

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,90 @@
 <template>
-  <h1>Header</h1>
+  <v-container>
+    <v-app-bar
+      id="rnr-app-bar-navbar"
+      app
+      color="transparent"
+      fixed
+      flat
+      :max-height="$vuetify.breakpoint.mobile ? 65 : 80"
+    >
+      <v-row class="d-flex align-center" align="center">
+        <h1 id="rnr-banner-title" class="mx-md-auto">
+          REWARDS & RECOGNITION
+        </h1>
+
+        <v-btn
+          id="rnr-leaderboard"
+          class="mx-md-4"
+          color="#999"
+          :ripple="ripple"
+          text
+        >
+          Leaderboard
+        </v-btn>
+        <v-btn
+          id="rnr-star-count"
+          class="mx-md-4"
+          color="#999"
+          icon
+          :ripple="ripple"
+          tile
+        >
+          <v-icon>
+            mdi-star
+          </v-icon>
+          0
+        </v-btn>
+        <v-btn
+          id="rnr-account"
+          class="mx-md-4 pr-md6"
+          color="#999"
+          icon
+          :ripple="ripple"
+          tile
+        >
+          <v-icon>
+            mdi-account
+          </v-icon>
+        </v-btn>
+      </v-row>
+    </v-app-bar>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    name: 'Header',
+    data() {
+      return {
+        clipped: false,
+        closeOnClick: true,
+        drawer: false,
+        miniVariant: false,
+        ripple: false
+      };
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  #rnr-app-bar-navbar {
+    border-bottom: solid thin #95989A !important;
+  }
+
+  #rnr-banner-title {
+    padding-left: 16.7% !important;
+  }
+
+  #rnr-leaderboard,
+  #rnr-star-count,
+  #rnr-account {
+    &:hover {
+      color: #fff !important;
+    }
+    &::before,
+    &::after {
+      background-color: transparent !important;
+    }
+  }
+</style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -6,37 +6,38 @@
     <h1 v-else>
       {{ otherError }}
     </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
   </v-app>
 </template>
 
 <script>
-export default {
-  layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null,
+  export default {
+    layout: 'empty',
+    props: {
+      error: {
+        type: Object,
+        default: null
+      }
     },
-  },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
+    data() {
+      return {
+        pageNotFound: '404 Not Found',
+        otherError: 'An error occurred'
+      };
+    },
+    head() {
+      const title = this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
+      return {
+        title
+      };
     }
-  },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title,
-    }
-  },
-}
+  };
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
-}
+  h1 {
+    font-size: 20px;
+  }
 </style>
