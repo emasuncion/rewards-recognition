a6f9308207d19ac9e5d82698ec832d253b42fcd5
Errol <errolmasuncion@gmail.com>
2021-02-24 15:59:06 +0800

    add footer component
    Signed-off-by: Errol <errolmasuncion@gmail.com>

---
+++ components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-footer
+    id="rnr-footer"
+    class="ml-auto mr-6 mb-6"
+    absolute
+    color="transparent"
+    max-width="100%"
+  >
+    <v-btn
+      class="pr-2"
+      :ripple="ripple"
+      text
+    >
+      ? HELP
+    </v-btn>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      ripple: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  #rnr-footer button {
+    &:hover {
+      color: #fff !important;
+    }
+    &::before,
+    &::after {
+      background-color: transparent !important;
+    }
+
+    color: #999;
+    right: 0;
+    position: absolute;
+  }
+</style>
