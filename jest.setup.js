0ede4f1d7e948ad946243784a7a88e6cdd7aaa15
Errol <errolmasuncion@gmail.com>
2021-02-26 16:50:57 +0800

    setup unit test and add UT for index page
    Signed-off-by: Errol <errolmasuncion@gmail.com>

---
+++ jest.setup.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import {config} from '@vue/test-utils'
+
+Vue.config.silent = true
+Vue.use(Vuetify)
+
+config.stubs['nuxt-link'] = true
+config.stubs['no-ssr'] = true
+config.mocks.$t = i => i
+config.mocks.switchLocalePath = i => i
+
+const vuetify = new Vuetify()
+
+export default vuetify
