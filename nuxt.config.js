34acbef840db0bcb749fe3c6f453bf43e9983b2a
Eleirold Asuncion <errolmasuncion@gmail.com>
2021-06-04 23:07:54 +0800

    fix css global files

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
     ]
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/scss/_colors.scss', '@/assets/scss/_mediaQueryMixins.scss'],
+  css: ['@/assets/css/main.css'],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
   // Auto import components: https://go.nuxtjs.dev/config-components
